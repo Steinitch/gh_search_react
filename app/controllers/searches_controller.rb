9105024fe7ee848439b8bf60795abd245a981e05
Paulito Spinach <paulstettnisch@gmail.com>
2018-05-11 22:10:28 -0500

    Add httparty gem, search form, and response in html table

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,13 @@
 class SearchesController < ApplicationController
   def index
-
+    @response ||= {}
+    @response['items'] = []
   end
 
   def create
+    query = params[:query]
+    @response = HTTParty.get("https://api.github.com/search/repositories?q=#{query}&sort=stars&order=desc")
 
+    render action: "index", response: @response
   end
 end
